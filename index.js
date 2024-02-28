3689e9618682d4ca908c93efb4125e08f1ba2c5a
prana.krushna <prana.krushna.dash@quodroid.com>
2024-02-28 17:17:17 +0530

    unroll and roll complete

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-function menuItemActivated(item){
-    switch(item){
-        case 'about':
-            document.getElementById("home-content").style.display='none';
-            document.getElementById("about-content").style.display='block';
-            document.getElementById("contact-content").style.display='none';
-        break;
-        default:
-            alert(`Pressed ${item}`);
-        break;
-    }
+function menuItemActivated(item) {
+  switch (item) {
+    case "about":
+      var roller = document.getElementById("roller_id");
+      roller.classList.remove("roller_animate");
+      roller.classList.add("roller_unroll_animate");
+
+      setTimeout(function () {
+        roller.classList.remove("roller_unroll_animate");
+        document.getElementById("home-content").style.display = "none";
+        document.getElementById("contact-content").style.display = "none";
+        
+      },2000);
+      
+
+      setTimeout(function () {
+        roller.classList.add("roller_animate");
+        document.getElementById("about-content").style.display = "block";
+      },2000);
+
+      break;
+    case "home":
+      var roller = document.getElementById("roller_id");
+      roller.classList.remove("roller_animate");
+
+      setTimeout(function () {
+        roller.classList.add("roller_animate");
+        document.getElementById("home-content").style.display = "block";
+        document.getElementById("about-content").style.display = "none";
+        document.getElementById("contact-content").style.display = "none";
+      },500);
+
+      break;
+    case "contact":
+      var roller = document.getElementById("roller_id");
+      roller.classList.remove("roller_animate");
+
+      setTimeout(function () {
+        roller.classList.add("roller_animate");
+        document.getElementById("home-content").style.display = "none";
+        document.getElementById("about-content").style.display = "none";
+        document.getElementById("contact-content").style.display = "block";
+      },500);
+
+      break;
+    default:
+      alert(`Pressed ${item}`);
+      break;
+  }
 }
